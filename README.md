6650d255acb15c1c6703ceb99181bb216a6788d6
Jeffrey Langerak <jeffrey@langerak.it>
2017-01-05 11:14:53 +0100

    Updated the README and added testing depency installer.

--- README.md
+++ README.md
@@ -13,6 +13,9 @@ creates backups as follows:
 * On Monday a full backup is created starting from the configured backup root
 * The rest of the weekdays a incremental backup (only the changed files) will
 be created based on the backup of yesterday (with exception of Monday).
+* In addition weekly and monthly retention can be enabled so you have a longer
+backup history. These are always full backups. The weekly backup is created on
+Monday and the monthly backup on the 1st of the month.
 
 # What are the local system requirements?
 The script has been tested on the following systems and is known to work:
@@ -22,9 +25,12 @@ The script has been tested on the following systems and is known to work:
 * Debian 5 up to Debian 8
 * ArchLinux and derivates
 
-For the scripts to work at least Bash 3 is needed. For better code handling I
+For the scripts to run at least Bash 3 is needed. For better code handling I
 will start shifting to Bash 4 so the requirements may change later on. For now
 with Bash 3 alot of obsolete OS'es will still work.
+
+For the backups itself at least rsync 3 is needed. Anything prior to version
+3 is not supported.
 
 # What are the remote system requirements?
 Basically anything that accepts SSH connections. This may vary from a regular
@@ -67,3 +73,18 @@ not limited to it:
 * arm
 * mips
 * powerpc
+
+* What are your future plans?
+The backupscript was originally designed to have a simple way of creating
+backups. As it is now more mature than when I started with it I want to
+expand the featureset of the scripts. One of the main features that are
+on the agenda is the support of the following database servers:
+
+* MongoDB
+* PostgreSQL
+
+Also the supported operating systems need to be expanded to:
+
+* (Open)Suse
+* Gentoo
+* Slackware
---
+++ installdeps-archlinux.sh
@@ -0,0 +1,196 @@
+#! /bin/bash
+#
+# This script contains the installation checks for the ArchLinux OS and is
+# part of the SuperBackup Suite created by Jeffrey Langerak.
+#
+# Bugs and/or features can be left at the repository below:
+# https://github.com/langerak/superbackup
+#
+# Starting the depency installation for ArchLinux:
+echo -ne "Checking for dialog: Please wait...     \r"
+if [ -x /usr/bin/dialog ];
+then
+        echo -ne "Checking for dialog: Present            \r"; echo
+logger -t superbackup_installer "Package dialog already installed"
+else
+    if pacman -Sqy --noconfirm dialog > /dev/null 2>&1
+    then
+        echo -ne "Checking for dialog: Installed       \r"; echo
+        logger -t superbackup_installer "Package dialog is now installed"
+    else
+        echo -ne "Checking for dialog: Failed          \r"; echo
+        echo -e "There was an error installing dialog! Installer will now abort!"
+        logger -t superbackup_installer "Package dialog not installed due to an error, exiting..."
+        exit
+    fi
+fi
+echo -ne "Checking for curl: Please wait...       \r"
+if [ -x /usr/bin/curl ];
+then
+    echo -ne "Checking for curl: Present               \r"
+    logger -t superbackup_installer "Package curl already installed"
+else
+    if pacman -Sqy --noconfirm curl > /dev/null 2>&1
+    then
+        echo -ne "Checking for curl: Installed         \r"
+        logger -t superbackup_installer "Package curl is now installed"
+    else
+        echo -ne "Checking for curl: Failed            \r"; echo
+        echo -e "There was an error installing curl! The installer will now abort!"
+        logger -t superbackup_installer "Package curl not installed due to an error, exiting..."
+        exit
+    fi
+fi
+echo -ne "Checking for rsync: Please wait...       \r"
+if [ -x /usr/bin/rsync ];
+then
+    echo -ne "Checking for rsync: Present               \r"; echo
+    logger -t superbackup_installer "Package rsync already installed"
+else
+    if pacman -Sqy --noconfirm rsync > /dev/null 2>&1
+    then
+        echo -ne "Checking for rsync: Installed         \r"; echo
+        logger -t superbackup_installer "Package rsync is now installed"
+    else
+        echo -ne "Checking for rsync: Failed            \r"; echo
+        echo -e "There was an error installing rsync! The installer will now abort!"
+        logger -t superbackup_installer "Package rsync not installed due to an error, exiting..."
+        exit
+    fi
+fi
+echo -ne "Checking for sendmail: Please wait...       \r"
+if [ -x /usr/bin/sendmail ];
+then
+    echo -ne "Checking for sendmail: Present               \r"; echo
+    logger -t superbackup_installer "Package sendmail already installed"
+else
+    if pacman -Sqy --noconfirm ssmtp > /dev/null 2>&1
+    then
+        echo -ne "Checking for sendmail: Installed         \r"; echo
+        logger -t superbackup_installer "Package sendmail is now installed"
+    else
+        echo -ne "Checking for sendmail: Failed            \r"; echo
+        echo -e "There was an error installing sendmail! The installer will now abort!"
+        logger -t superbackup_installer "Package sendmail not installed due to an error, exiting..."
+        exit
+    fi
+fi
+echo -ne "Checking for bc: Please wait...       \r"
+if [ -x /usr/bin/bc ];
+then
+    echo -ne "Checking for bc: Present               \r"; echo
+    logger -t superbackup_installer "Package bc already installed"
+else
+    if pacman -Sqy --noconfirm bc > /dev/null 2>&1
+    then
+        echo -ne "Checking for bc: Installed         \r"; echo
+        logger -t superbackup_installer "Package bc is now installed"
+    else
+        echo -ne "Checking for bc: Failed            \r"; echo
+        echo -e "There was an error installing bc! The installer will now abort!"
+        logger -t superbackup_installer "Package bc not installed due to an error, exiting..."
+        exit
+    fi
+fi
+echo -ne "Checking for expect: Please wait...       \r"
+if [ -x /usr/bin/expect ];
+then
+    echo -ne "Checking for expect: Present               \r"; echo
+    logger -t superbackup_installer "Package expect already installed"
+else
+    if pacman -Sqy --noconfirm expect > /dev/null 2>&1
+    then
+        echo -ne "Checking for expect: Installed         \r"; echo
+        logger -t superbackup_installer "Package expect is now installed"
+    else
+        echo -ne "Checking for expect: Failed            \r"; echo
+        echo -e "There was an error installing expect! The installer will now abort!"
+        logger -t superbackup_installer "Package expect not installed due to an error, exiting..."
+        exit
+    fi
+fi
+echo -ne "Checking for Perl: Please wait...       \r"
+if [ -x /usr/bin/perl ];
+then
+    echo -ne "Checking for Perl: Present               \r"; echo
+    logger -t superbackup_installer "Package perl already installed"
+else
+    if pacman -Sqy --noconfirm perl > /dev/null 2>&1
+    then
+        echo -ne "Checking for Perl: Installed         \r"; echo
+        logger -t superbackup_installer "Package perl is now installed"
+    else
+        echo -ne "Checking for Perl: Failed            \r"; echo
+        echo -e "There was an error installing Perl! The installer will now abort!"
+        logger -t superbackup_installer "Package perl not installed due to an error, exiting..."
+        exit
+    fi
+fi
+echo -ne "Checking for mc: Please wait...       \r"
+if [ -x /usr/bin/mc ];
+then
+    echo -ne "Checking for mc: Present               \r"; echo
+    logger -t superbackup_installer "Package mc already installed"
+else
+    if pacman -Sqy --noconfirm mc > /dev/null 2>&1
+    then
+        echo -ne "Checking for mc: Installed         \r"; echo
+        logger -t superbackup_installer "Package mc is now installed"
+    else
+        echo -ne "Checking for mc: Failed            \r"; echo
+        echo -e "There was an error installing mc! The installer will now abort!"
+        logger -t superbackup_installer "Package mc not installed due to an error, exiting..."
+        exit
+    fi
+fi
+echo -ne "Checking for sshfs: Please wait...       \r"
+if [ -x /usr/bin/sshfs ];
+then
+    echo -ne "Checking for sshfs: Present               \r"; echo
+    logger -t superbackup_installer "Package sshfs already installed"
+else
+    if pacman -Sqy --noconfirm sshfs > /dev/null 2>&1
+    then
+        echo -ne "Checking for sshfs: Installed         \r"; echo
+        logger -t superbackup_installer "Package sshfs is now installed"
+    else
+        echo -ne "Checking for sshfs: Failed            \r"; echo
+        echo -e "There was an error installing sshfs! The installer will now abort!"
+        logger -t superbackup_installer "Package sshfs not installed due to an error, exiting..."
+        exit
+    fi
+fi
+echo -ne "Checking for sshfs: Please wait...       \r"
+if [ -x /usr/bin/sshfs ];
+then
+    echo -ne "Checking for sshfs: Present               \r"; echo
+    logger -t superbackup_installer "Package sshfs already installed"
+else
+    if pacman -Sqy --noconfirm sshfs > /dev/null 2>&1
+    then
+        echo -ne "Checking for sshfs: Installed         \r"; echo
+        logger -t superbackup_installer "Package sshfs is now installed"
+    else
+        echo -ne "Checking for sshfs: Failed            \r"; echo
+        echo -e "There was an error installing sshfs! The installer will now abort!"
+        logger -t superbackup_installer "Package sshfs not installed due to an error, exiting..."
+        exit
+    fi
+fi
+echo -ne "Checking for nano: Please wait...       \r"
+if [ -x /usr/bin/nano ];
+then
+    echo -ne "Checking for nano: Present               \r"; echo
+    logger -t superbackup_installer "Package nano already installed"
+else
+    if pacman -Sqy --noconfirm nano > /dev/null 2>&1
+    then
+        echo -ne "Checking for nano: Installed         \r"; echo
+        logger -t superbackup_installer "Package nano is now installed"
+    else
+        echo -ne "Checking for nano: Failed            \r"; echo
+        echo -e "There was an error installing nano! The installer will now abort!"
+        logger -t superbackup_installer "Package nano not installed due to an error, exiting..."
+        exit
+    fi
+fi
